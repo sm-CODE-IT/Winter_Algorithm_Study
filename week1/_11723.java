9840b45e8464c12395d996e31381ca45abd629ba
jun02160 <jun020216@naver.com>
2023-01-02 22:16:21 +0900

    [UPDATE] 코드 주석 추가

--- week1/_11723.java
+++ week1/_11723.java
@@ -34,27 +34,27 @@ public class _11723 {
     public static void commands(String cmd) {
         switch (cmd) {
             case "all":
-                bitMask = ~0;
+                bitMask = ~0;  // 1~20의 모든 비트를 1로 켜준다,
                 break;
             case "empty":
-                bitMask = 0;
+                bitMask = 0;  // 1~20의 모든 비트를 0으로 끈다,
                 break;
         }
     }
 
     public static void commands(String cmd, int x) {
         switch (cmd) {
-            case "add":
-                bitMask = bitMask | 1 << (x-1);
+            case "add":   // ex. add 1, add 2, add 3 의 결과는 -> 111
+                bitMask = bitMask | 1 << (x-1); // 1 << (x-1): x-1번 비트만 1, 나머지 비트는 0으로 만들고 | 연산으로 나머지는 원래 상태를 유지한 채 x-1번 비트를 1로 바꿔준다,
                 break;
             case "remove":
-                bitMask = bitMask & ~(1 << (x-1));
+                bitMask = bitMask & ~(1 << (x-1)); // 1 << (x-1): 추가 시와 동일하기 x-1번 비트를 1로 만들어주고, & 연산을 진행하여 x-1번만 0으로 바뀌고 나머지는 유지되도록 한다.
                 break;
             case "check":
                 sb.append(((bitMask & 1 << (x-1)) == 1 << (x-1) ? 1 : 0) + "\n");
                 break;
             case "toggle":
-                bitMask = bitMask ^ 1 << (x-1);
+                bitMask = bitMask ^ 1 << (x-1);  // ^ 연산을 통해 x-1번 자리의 비트를 1->0. 0->1로 바꿔준다.
                 break;
         }
     }
